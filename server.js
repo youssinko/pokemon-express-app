0e6a14da793d322a7cdd5c95dfa3dbf021d42b91
Rania Elnaggar <yousef_amir1@hotmail.com>
2022-11-16 11:02:49 -0500

    changed index to ID

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.get('/',(req, res)=>{
 app.get('/pokemon', (req, res)=>{
     res.render('Index',{pokemon})
 })
-// app.get('/pokemon/:id',(req,res)=>{
-//     res.send(req.params.id)
-// })
-app.get('/pokemon/:indexOfPokemon', (req ,res)=>{
-    res.render("Show" , pokemon[req.params.indexOfPokemon])
+app.get('/pokemon/:id',(req,res)=>{
+    res.render('show' ,pokemon[req.params.id])
 })
+// app.get('/pokemon/:indexOfPokemon', (req ,res)=>{
+//     res.render("Show" , pokemon[req.params.indexOfPokemon])
+// })
 app.listen(PORT,() =>{
     console.log(`Listening on port:${PORT}`)
 })
